6d010a77e285342c55dd94f618ae6a5608b01daa
IT21308666 <99382396+IT21308666@users.noreply.github.com>
2022-03-25 09:12:47 +0000

    i complete my tutorial 3

--- exercise04.cpp
+++ exercise04.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 void print(int len, int wth);
@@ -18,4 +18,31 @@ void print(int len, int wth) {
         << ", Width  : " << wth << endl;
 }
 
-// Implement the Input Function here
+// Implement the Input Function here*/
+#include<iostream>
+using namespace std;
+
+void print(int len,int wth);
+void input(int & len, int & wth);
+
+//Do not change the main()function
+int main(){
+  int length = 10,width = 5;
+  input(length,width);
+  print(length,width);
+  return 0;
+}
+
+//Do not change the print()function
+void print(int len,int wth){
+  cout<<"Length : "<<len<<"Width : "<<wth<<endl;
+}
+
+void input(int & len,int &wth)
+{
+  len = 100;
+  wth = 20;
+
+  cout<<len<<endl;
+  cout<<wth<<endl;
+}
